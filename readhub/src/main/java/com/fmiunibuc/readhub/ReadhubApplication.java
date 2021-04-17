dfa0ff7866588a67177fed8e62e27c0521097468
dianagrigore <diana.grigore12@yahoo.com>
2021-04-17 16:54:55 +0300

    initial commit

---
+++ readhub/src/main/java/com/fmiunibuc/readhub/ReadhubApplication.java
@@ -0,0 +1,13 @@
+package com.fmiunibuc.readhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadhubApplication.class, args);
+	}
+
+}
---
+++ readhub/src/test/java/com/fmiunibuc/readhub/ReadhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmiunibuc.readhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
