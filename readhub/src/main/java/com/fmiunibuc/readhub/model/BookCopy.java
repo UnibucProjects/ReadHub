fb01856a9b93aac7677bcd17b2669ba0f5242878
dianagrigore <diana.grigore12@yahoo.com>
2021-05-15 10:12:36 +0300

    added BookCopy class for managing many to many problem in database and initial diagram + refactored code to match addition

--- readhub/src/main/java/com/fmiunibuc/readhub/model/BookCopy.java
+++ readhub/src/main/java/com/fmiunibuc/readhub/model/BookCopy.java
@@ -1,26 +1,26 @@
 package com.fmiunibuc.readhub.model;
 
-import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Entity
-public class Book {
-    @Id
+public class BookCopy {
     @GeneratedValue
+    @Id
     private Long id;
-    @NotNull
-    private String name;
-    private String author;
-    @OneToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL)
-    private List<BookCopy> copyList;
+    private String status;
+    private Long pagesRead;
+    private Integer rating;
+    @ManyToOne(cascade= CascadeType.PERSIST)
+    private Book bookType;
+    @ManyToOne(cascade=CascadeType.PERSIST)
+    private Shelf shelf;
 }
--- readhub/src/main/java/com/fmiunibuc/readhub/model/User.java
+++ readhub/src/main/java/com/fmiunibuc/readhub/model/User.java
@@ -5,10 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 import java.util.List;
 
 @Data
@@ -21,6 +18,6 @@ public class User {
     @NotNull
     private String name;
     private String email;
-    @ManyToMany(fetch = FetchType.EAGER)
-    private List<Library> libraryList;
+    @OneToOne(cascade=CascadeType.ALL, fetch = FetchType.EAGER)
+    private Library library;
 }
